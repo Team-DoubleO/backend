3408ad3cc7f4daec331c9a90fe84b219115158eb
Yongbum Kim <dt10002@naver.com>
2025-11-27 16:41:00 +0900

    init: project init

---
+++ src/test/java/com/spots/SpotsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

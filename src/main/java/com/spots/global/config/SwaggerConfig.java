06866529c7192bdf300415da32c56f7999f3a5d8
Yongbum Kim <dt10002@naver.com>
2025-12-02 02:22:16 +0900

    feat: swagger 의존성 추가 및 config 설정
    - validation과 swagger 의존성을 추가했습니다.
    - swagger 웹 연동을 위한 config 파일을 작성했습니다.
    
    issue #2

---
+++ src/main/java/com/spots/global/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.spots.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI openAPI() {
+    return new OpenAPI()
+        .info(info);
+  }
+
+  Info info = new Info()
+      .title("KSPO Pulbic Data Competition API")
+      .version("0.0.1")
+      .description("<h3>KSPO Public Data Competition</h3>");
+}

bfde0dc95463afcdf0d3a234e6269ace97b1c55c
Yongbum Kim <dt10002@naver.com>
2025-12-03 01:03:33 +0900

    feat: profile 설정 분리
    - 배포 환경과 로컬 환경을 분리하기 위해서 profile 설정을 분리했습니다.
    - docker-compose 파일에서 배포 환경에서는 prod profile을 적용하도록 수정했습니다.
    - swagger 서버 URL을 분리한 profile 설정과 동기화했습니다.
    
    issue #3

--- src/main/java/com/spots/global/config/SwaggerConfig.java
+++ src/main/java/com/spots/global/config/SwaggerConfig.java
@@ -2,16 +2,24 @@ package com.spots.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+  @Value("${swagger.server-url}")
+  private String SERVER_URL;
+
   @Bean
   public OpenAPI openAPI() {
     return new OpenAPI()
-        .info(info);
+        .servers(List.of(
+            new Server().url(SERVER_URL)
+        )).info(info);
   }
 
   Info info = new Info()
